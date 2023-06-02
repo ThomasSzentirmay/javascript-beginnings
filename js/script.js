c757d411602dda291ac2bd5c79c89893dd4fccce
Thomas Szentirmay <tlszent@outlook.com>
2023-06-02 14:11:24 -0400

    started objects

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
-var students = ['Matthew', 'FSF']
+var oldStudentData = ['Matthew', 'FSF']
+var studentData = {
+    name: 'Matthew',
+    course: 'FSF',
+    university: 'Rutgers',
+    location: 'Jersey',
+    age: 26,
+    someFunc: function () {
+        console.log('crazy!');
+    },
+    printInfo: function () {
+        console.log('Name:', this.name);
+    }
+};
 
-var studentName = students[0];
+studentData.someFunc();
+studentData.printInfo();
 
-var studentCourse = students[1];
+// var studentName = students[0];
 
-console.log(studentName, studentCourse);
+// var studentCourse = students[1];
+
+// to pull the value from an object, state the object name (studentData), and then . notation and the property name (.name)
+
+// var studentName = studentData.name;
+// var studentCourse = studentData.course;
+
+// console.log(studentData.age);
+
+// arrays store lists of data - objects store organised data
