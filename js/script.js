310acde652e7ffee6309db301346a47b5b5f38cc
Thomas Szentirmay <tlszent@outlook.com>
2023-06-05 11:56:37 -0400

    object lessons

--- js/script.js
+++ js/script.js
@@ -33,7 +33,73 @@ var users = [
     }
 ];
 
-console.log(users[2].healthcare.medications.types[1].pillSize);
+// console.log(users[2].healthcare.medications.types[1].pillSize);
+
+// work your way down the data levels to console log the wanted value
+// anytime you use a . and a prop name (.medications), the value before it must be of type object
+
+
+
+
+
+
+
+// var str = 'some string';
+
+// str = str.slice(3);
+
+
+// console.log(str);
+// console.log(str.length);
+
+
+// every value is an object is JavaScript (OOP)
+// why did they use objects for the JS toolset? > we have a value and a toolset (str = 'some string'). and so then if the str is represented by an object, we can easily pull data and info about the string
+// if str WASN'T an object, we wouldn't be able to use .length to get the strings length
+
+
+
+
+
+var jd = {
+    name: 'JD',
+    age: 43,
+    hobbies: ['pickleball', 'fishing'],
+    haveBirthday: function () {
+        this.age++;
+    },
+    printSomething: function(txt) {
+        console.log(txt);
+    },
+    printAge: function () {
+        console.log(this.age);
+    },
+    printHobbiesByIndex: function(index) {
+        console.log(this.hobbies[index]);
+    }
+}
+
+jd.printHobbiesByIndex(0);
+
+// jd.printSomething('print me!');
+
+// jd.haveBirthday();
+
+// jd.printAge();
+
+
+// haveBirthday: function () {    is an anonymous function. it is un-named, but can still be called because the function is stored in the haveBirthday property. The un-named function has a name!
+// 'this' is a keyword we can use to reference the parent, instead of typing the parent name before the .
+
+
+
+
+
+
+
+
+
+
 
 
 
