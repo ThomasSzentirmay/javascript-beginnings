e6cafd1ea4f478ef496cc6969e520d9252403cb8
Thomas Szentirmay <tlszent@outlook.com>
2023-06-06 13:08:01 -0400

    looked at DOM and styles within JS

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 var body = document.body;
-var btnElObject = document.getElementById('start');
+var btnElObject = document.querySelector('#start');
+var mainEl = document.querySelector('.main-content');
+
+console.log(mainEl);
 
 // console.log(btnElObject);
 // btnElObject.innerText = 'something else';
-// btnElObject.style.backgroundColor = '#5f1a9a';
-// btnElObject.style.color = '#fff';
+btnElObject.style.backgroundColor = '#5f1a9a';
+btnElObject.style.color = '#fff';
+mainEl.style.backgroundColor = 'red';
+
+mainEl.setAttribute('id', 'something');
+
 
 btnElObject.addEventListener('click', function () {
     body.classList.add('explosion');
 });
+
+
