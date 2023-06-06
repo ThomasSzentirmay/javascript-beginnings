868deb2a95c38cab32e94b1d90b3687f4306adad
Thomas Szentirmay <tlszent@outlook.com>
2023-06-06 11:25:06 -0400

    button functionality JS

--- js/script.js
+++ js/script.js
@@ -1,55 +1,11 @@
-// API is a tool set to interact with something. As a developer, that something is code.
-// The web API is the JavaScript sub-set of functions and objects for us to work with the browser
+var body = document.body;
+var btnElObject = document.getElementById('start');
 
+// console.log(btnElObject);
+// btnElObject.innerText = 'something else';
+// btnElObject.style.backgroundColor = '#5f1a9a';
+// btnElObject.style.color = '#fff';
 
-// DOM
-// Window  >  Browser window  &&  Document  >  </html>
-
-
-// setInterval(function () {
-//     console.log('Run this code');
-// }, 1000);
-
-// function myFunc (one, two, callBackFunc) {
-//     callBackFunc('some callback argument');
-// }
-
-// myFunc(10, 'some str', function (str) { console.log(str) });
-
-
-
-
-// DECLARATION FUNCTION:
-function getProduct(a, b) {
-    return a * b;
-}
-
-getProduct(5, 5);
-
-
-
-function doSomething(whatAmI, blah) {
-    whatAmI('arg passed to callback');
-    console.log(blah);
-}
-
-doSomething(function(youGotThis) {
-    console.log(youGotThis);
-}, 10);
-
-
-
-// EXPRESSION FUNCTION:
-// var another = function () {
-// }
-
-// another();
-
-
-// METHOD FUNCTION:
-// var person = {
-//     haveBirthday: function() {
-//     }
-// }
-
-// person.haveBirthday();
+btnElObject.addEventListener('click', function () {
+    body.classList.add('explosion');
+});
