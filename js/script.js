8c12f57620e38c6cace560207da409e5e273a124
Thomas Szentirmay <tlszent@outlook.com>
2023-06-06 14:32:57 -0400

    injecting HTML

--- js/script.js
+++ js/script.js
@@ -1,20 +1,44 @@
 var body = document.body;
 var btnElObject = document.querySelector('#start');
-var mainEl = document.querySelector('.main-content');
+var mainSectionEl = document.querySelector('.main-content section');
+var questionData = [
+    {
+        question: 'What does the acronym D.O.M. stand for?',
+        choices: ['Document on Memory', 'Dog Only Members', 'Dancing On Mercury', 'Document Object Model'],
+        answer: 'Document Object Model'
+    },
+    {
+        question: 'Should we refer to JavaScript as Java?',
+        choices: ['Maybe', 'Yes', 'No', 'Only on Saturdays'],
+        answer: 'No'
+    }
+];
 
-console.log(mainEl);
+for (var index = 0; index < questionData.length; index++) {
+    var questionObj = questionData[index];
+    var questionDiv = document.createElement('div');
+    var h2 = document.createElement('h2');
+    var p = document.createElement('p');
 
-// console.log(btnElObject);
-// btnElObject.innerText = 'something else';
-btnElObject.style.backgroundColor = '#5f1a9a';
-btnElObject.style.color = '#fff';
-mainEl.style.backgroundColor = 'red';
+    h2.innerText = questionObj.question;
+    p.innerText = questionObj.answer;
 
-mainEl.setAttribute('id', 'something');
+    questionDiv.append(h2);
+    questionDiv.append(p);
+
+    // questionDiv.innerHTML = `
+    // <h2>${questionObj.question}</h2>
+    // <p>Answer: ${questionObj.answer}</p>
+    // `;
+
+    // mainSectionEl.append(questionDiv);
+}
 
 
-btnElObject.addEventListener('click', function () {
-    body.classList.add('explosion');
-});
+
+
+// btnElObject.addEventListener('click', function () {
+//     body.classList.add('explosion');
+// });
 
 
