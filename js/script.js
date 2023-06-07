9956332a16a9ba42a41bbaaac7e3d1b58f002961
Thomas Szentirmay <tlszent@outlook.com>
2023-06-07 14:31:07 -0400

    JD challenge progress

--- js/script.js
+++ js/script.js
@@ -1,44 +1,42 @@
-var body = document.body;
-var btnElObject = document.querySelector('#start');
-var mainSectionEl = document.querySelector('.main-content section');
-var questionData = [
-    {
-        question: 'What does the acronym D.O.M. stand for?',
-        choices: ['Document on Memory', 'Dog Only Members', 'Dancing On Mercury', 'Document Object Model'],
-        answer: 'Document Object Model'
-    },
-    {
-        question: 'Should we refer to JavaScript as Java?',
-        choices: ['Maybe', 'Yes', 'No', 'Only on Saturdays'],
-        answer: 'No'
-    }
-];
+// var link = document.querySelector('#link');
+// var div = document.querySelector('div');
 
-for (var index = 0; index < questionData.length; index++) {
-    var questionObj = questionData[index];
-    var questionDiv = document.createElement('div');
-    var h2 = document.createElement('h2');
-    var p = document.createElement('p');
+// function triggered (eventObj) {
+//     if (eventObj.target.tagName === 'BUTTON') {
+//         eventObj.target.innerText = 'Clicked';
+//     }
+// };
 
-    h2.innerText = questionObj.question;
-    p.innerText = questionObj.answer;
+// div.addEventListener('click', triggered);
 
-    questionDiv.append(h2);
-    questionDiv.append(p);
 
-    // questionDiv.innerHTML = `
-    // <h2>${questionObj.question}</h2>
-    // <p>Answer: ${questionObj.answer}</p>
-    // `;
 
-    // mainSectionEl.append(questionDiv);
-}
+// function stopTheDefault(eventObj) {
+//     eventObj.preventDefault();
+
+
+//     eventObj.target.innerText = 'Something Else'
+// };
+
+// link.addEventListener('click', stopTheDefault);
 
 
 
 
-// btnElObject.addEventListener('click', function () {
-//     body.classList.add('explosion');
-// });
+
+// Target all the li's in the ul and add an event listener to each li that console logs the innerText of the li
+
+// Target the ul and add an event listener that console logs the innerText of the li that was clicked
 
 
+var ul = document.querySelector('ul');
+
+function liListener(e) {
+
+    if (e.target.tagName === 'LI') {
+    console.log(e.target.innerText);
+    };
+
+};
+
+ul.addEventListener('click', liListener)
