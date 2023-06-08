f19e900bbfce825b094de22c30cc6b50f0f8ba4a
Thomas Szentirmay <tlszent@outlook.com>
2023-06-08 14:14:44 -0400

    finished basic keyboard events and localStorage usage

--- js/script.js
+++ js/script.js
@@ -1,6 +1,29 @@
 var formEl = document.querySelector('#user-form');
 var emailInput = document.querySelector('#email-input');
 var passwordInput = document.querySelector('#pass-input');
+var emailOutput = document.querySelector('#user-email');
+var passOutput = document.querySelector('#user-pass');
+
+// Retrieve user data from localStorage
+function getUserData () {
+    var data = {};
+    
+    data.email = localStorage.getItem('email');
+    data.password = localStorage.getItem('password');
+
+    return data;
+};
+
+
+// Outputs the user information to the window
+function showUserData () {
+    var userData = getUserData();
+    
+    emailOutput.innerText = `Email: ${userData.email}`;
+    passOutput.innerText = `Password: ${userData.password}`;
+
+};
+
 
 function getUserInput(eventObj) {
     eventObj.preventDefault();
@@ -10,9 +33,13 @@ function getUserInput(eventObj) {
 
     localStorage.setItem('email', email);
     localStorage.setItem('password', password);
+
+    showUserData();
 };
 
+// Initial process or app start tasks
 formEl.addEventListener('submit', getUserInput);
+showUserData();
 
 
 
