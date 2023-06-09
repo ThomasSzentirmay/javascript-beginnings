510dba4325d797340c2b2d976db1610a6781075d
Thomas Szentirmay <tlszent@outlook.com>
2023-06-09 12:26:29 -0400

    finished localstorage advanced

--- js/script.js
+++ js/script.js
@@ -12,10 +12,69 @@ function addClick() {
     clickOutput.innerText = 'Clicks: ' + clicks;
 };
 
+function clearData() {
+    input.value = '';
+    clicks = 0;
+    clickOutput.innerText = 'Clicks: 0';
+};
+
+function getUserData() {
+    var rawData = localStorage.getItem('users');
+    var parsed = JSON.parse(rawData) || [];
+
+    return parsed;
+};
+
+function saveUserData(arr) {
+    var jsonVal = JSON.stringify(arr);
+
+    localStorage.setItem('users', jsonVal);
+};
+
 function saveUserClicks(eventObj) {
     if (eventObj.keyCode === 13) {
-        localStorage.setItem();
+        var userScore = {
+            name: input.value,
+            clicks: clicks
+        };
+
+        var usersArray = getUserData();
+        usersArray.push(userScore);
+
+        saveUserData(usersArray);
+
+        showScores();
+
+        clearData();
     };
+};
+
+function showScores() {
+    var users = getUserData();
+    var scoresDiv = document.querySelector('.scores');
+
+    scoresDiv.innerHTML = '';
+
+    if (users.length === 0) {
+        scoresDiv.innerHTML = '<p>No scores have been added</p>';
+    };
+
+    for (var userObj of users) {
+        var div = document.createElement('div');
+        div.classList.add('score')
+
+        var h3 = document.createElement('h3');
+        h3.innerText = `Name: ${userObj.name}`;
+        div.append(h3);
+
+        var p = document.createElement('p');
+        p.innerText = `Clicks: ${userObj.clicks}`;
+        div.append(p);
+
+        scoresDiv.append(div);
+
+        // console.log(div);
+    }
 };
 
 clickBtn.addEventListener('click', addClick);
